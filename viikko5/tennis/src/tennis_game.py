a1906d37ea665481f8b1ae6c43df8457ec9ffd2e
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-11-29 15:58:08 +0200

    Revert "Viikko5/tennis: Game_score"
    This reverts commit 63a8eaf65664167210f437b8777a1791563d122a.

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -7,7 +7,6 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1 = Player(player1_name)
         self.player2 = Player(player2_name)
-        self.game_score = ""
 
     def won_point(self, player_name):
         if self.player1.name == player_name:
@@ -22,39 +21,39 @@ class TennisGame:
 
         if self.player1.score == self.player2.score:
             if self.player1.score == 0:
-                self.game_score = "Love-All"
+                score = "Love-All"
             elif self.player1.score == 1:
-                self.game_score = "Fifteen-All"
+                score = "Fifteen-All"
             elif self.player1.score == 2:
-                self.game_score = "Thirty-All"
+                score = "Thirty-All"
             else:
-                self.game_score = "Deuce"
+                score = "Deuce"
         elif self.player1.score >= 4 or self.player2.score >= 4:
             minus_result = self.player1.score - self.player2.score
 
             if minus_result == 1:
-                self.game_score = "Advantage player1"
+                score = "Advantage player1"
             elif minus_result == -1:
-                self.game_score = "Advantage player2"
+                score = "Advantage player2"
             elif minus_result >= 2:
-                self.game_score = "Win for player1"
+                score = "Win for player1"
             else:
-                self.game_score = "Win for player2"
+                score = "Win for player2"
         else:
             for i in range(1, 3):
                 if i == 1:
                     temp_score = self.player1.score
                 else:
-                    self.game_score = self.game_score + "-"
+                    score = score + "-"
                     temp_score = self.player2.score
 
                 if temp_score == 0:
-                    self.game_score = self.game_score + "Love"
+                    score = score + "Love"
                 elif temp_score == 1:
-                    self.game_score = self.game_score + "Fifteen"
+                    score = score + "Fifteen"
                 elif temp_score == 2:
-                    self.game_score = self.game_score + "Thirty"
+                    score = score + "Thirty"
                 elif temp_score == 3:
-                    self.game_score = self.game_score + "Forty"
+                    score = score + "Forty"
 
         return score
