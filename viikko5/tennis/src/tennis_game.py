63a8eaf65664167210f437b8777a1791563d122a
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-11-29 15:56:22 +0200

    Viikko5/tennis: Game_score

--- viikko5/tennis/src/tennis_game.py
+++ viikko5/tennis/src/tennis_game.py
@@ -7,6 +7,7 @@ class TennisGame:
     def __init__(self, player1_name, player2_name):
         self.player1 = Player(player1_name)
         self.player2 = Player(player2_name)
+        self.game_score = ""
 
     def won_point(self, player_name):
         if self.player1.name == player_name:
@@ -21,39 +22,39 @@ class TennisGame:
 
         if self.player1.score == self.player2.score:
             if self.player1.score == 0:
-                score = "Love-All"
+                self.game_score = "Love-All"
             elif self.player1.score == 1:
-                score = "Fifteen-All"
+                self.game_score = "Fifteen-All"
             elif self.player1.score == 2:
-                score = "Thirty-All"
+                self.game_score = "Thirty-All"
             else:
-                score = "Deuce"
+                self.game_score = "Deuce"
         elif self.player1.score >= 4 or self.player2.score >= 4:
             minus_result = self.player1.score - self.player2.score
 
             if minus_result == 1:
-                score = "Advantage player1"
+                self.game_score = "Advantage player1"
             elif minus_result == -1:
-                score = "Advantage player2"
+                self.game_score = "Advantage player2"
             elif minus_result >= 2:
-                score = "Win for player1"
+                self.game_score = "Win for player1"
             else:
-                score = "Win for player2"
+                self.game_score = "Win for player2"
         else:
             for i in range(1, 3):
                 if i == 1:
                     temp_score = self.player1.score
                 else:
-                    score = score + "-"
+                    self.game_score = self.game_score + "-"
                     temp_score = self.player2.score
 
                 if temp_score == 0:
-                    score = score + "Love"
+                    self.game_score = self.game_score + "Love"
                 elif temp_score == 1:
-                    score = score + "Fifteen"
+                    self.game_score = self.game_score + "Fifteen"
                 elif temp_score == 2:
-                    score = score + "Thirty"
+                    self.game_score = self.game_score + "Thirty"
                 elif temp_score == 3:
-                    score = score + "Forty"
+                    self.game_score = self.game_score + "Forty"
 
         return score
