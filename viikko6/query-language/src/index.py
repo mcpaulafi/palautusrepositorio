7d2e9b85ed632b2ae168aaff623f1a88a9b0c8eb
Paula Meuronen <paula.meuronen@helsinki.fi>
2024-12-06 14:56:52 +0200

    Viikko6: Tehtävä2

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,11 +1,13 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+from matchers import And, HasAtLeast, PlaysIn, Not, HasFewerThan, All
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
+
+    print("Has at least 5 goals and 20 assists")
 
     matcher = And(
         HasAtLeast(5, "goals"),
@@ -15,6 +17,30 @@ def main():
 
     for player in stats.matches(matcher):
         print(player)
+
+    print("\nNOT Has at least 2 goals")
+
+    matcher = And(
+        Not(HasAtLeast(2, "goals")),
+        PlaysIn("NYR")
+    )
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("\nHas fewer than 2 goals")
+
+    matcher = And(
+        HasFewerThan(2, "goals"),
+        PlaysIn("NYR")
+    )
+    for player in stats.matches(matcher):
+        print(player)
+
+    print("\nAll")
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
 
 
 if __name__ == "__main__":
--- viikko6/query-language/src/matchers.py
+++ viikko6/query-language/src/matchers.py
@@ -27,3 +27,35 @@ class HasAtLeast:
         player_value = getattr(player, self._attr)
 
         return player_value >= self._value
+
+class HasFewerThan:
+    """HasAtLeast-komennon negaatio eli esim. on vähemmän kuin 10 maalia"""
+    def __init__(self, value, attr):
+        self._value = value
+        self._attr = attr
+
+    def test(self, player):
+        player_value = getattr(player, self._attr)
+
+        return player_value < self._value
+
+class All:
+    """tosi kaikille pelaajille"""
+#    stats.matches(All())
+    def __init__(self):
+        pass
+
+    def test(self, player):
+        return True
+
+class Not:
+    """parametrina olevan ehdon negaatio"""
+    def __init__(self, *matchers):
+        self._matchers = matchers
+
+    def test(self, player):
+        for matcher in self._matchers:
+            if not matcher.test(player):
+                return True
+
+        return False
